ecbe1cef22be4c6af659194c7caaf1e464c04de0
nicoleo97 <73941175+nicoleo97@users.noreply.github.com>
2025-10-28 14:00:57 +0000

    edit

--- pages/FunktionenAllgemein.py
+++ pages/FunktionenAllgemein.py
@@ -30,3 +30,11 @@ ax.plot(x,y)
 
 # Graph auf Streamlit ausgeben
 st.pyplot(fig)
+
+
+### QUIZ ###
+c_links, c_rechts = st.container()
+nullstellen_frage = st.radio("Wieviele Nullstellen hat diese Funktion?",[0,1,2,3,4])
+monotonie_frage = st.radio("Wie ist das Monotonieverhalten dieser Funktion?",["streng monoton steigend","streng monoton fallend","beides"])
+extremstellen_frage = st.radio("Welche Extrempunkte hat die Funktion?",["Maximum","Minimum","Beide","Keine"])
+
